5ec1246a51b58e14f4b93a5b4a11263b0365c712
Mariana Licona <m.licona@abtis.de>
2021-10-31 01:19:09 +0200

    !!!

--- formscript.php
+++ formscript.php
@@ -1,16 +1,20 @@
-<?php $nombreF = $_POST['nombreF'];
-$rfcF = $_POST['rfcF'];
-$direccionF = $_POST['direccionF'];
-$telefonoF = $_POST['telefonoF'];
-$correoF = $_POST['correoF'];
-$recipient = "dejmariana@gmail.com";
-$subject = "Mi tarea";
-$mailheader = "De: $email \r\n";
-mail($recipient, $subject, $nombreF,$rfcF,$telefonoF,$correoF $mailheader) or die("Error!");
-echo "Gracias profe, me gusto esta tarea";
-?>
+<html>
+<body>
+
+<p>Hola <?php echo $_GET["nombreF"]; ?><br>
+          Tu RFC es: <?php echo $_GET["rfcF"]; ?> <br>
+          Tu direccion es: <?php echo $_GET["direccionF"]; ?> <br>
+          Tu Telefono es: <?php echo $_GET["telefonoF"]; ?><br>
+          Tu Correo es: <?php echo $_GET["correoF"]; ?><br>
+          Gracias, ya te registramos.
+</p>
+
+</body>
+</html>
+
 
 <!--Mariana de San Juan Licona Aguilar.
    ES162006875 . 
    Desjmariana@nube.unadmexico.mx
+   Grupo: I – DPW1-B2
 -->
